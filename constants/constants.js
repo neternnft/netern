4701e80cba9b3a06a4998fc640c3b5db351cbc97
neternnft <netern.nft@gmail.com>
2022-05-05 21:33:04 +0200

    gg

--- constants/constants.js
+++ constants/constants.js
@@ -13,7 +13,7 @@
 */
 
 // ---GENERAL---
-export const logo = "/images/logo.svg"
+export const logo = "/images/logo.png"
 export const logoAlt = "nft project logo"
 export const mobileMenuHeading = "Useful Links:"
 
@@ -64,7 +64,7 @@ export const backCard = {
   profile:
     "https://lh3.googleusercontent.com/uRNiQu5_VpjhHoYy_QTvqOpIWs5QvSJ36hCrsGV4jP2YDJqIwfBVtR_XALDmmHHyFks6FC919AzvXv0J1AT4WlSEGM6VIODNt07snw=s130",
   image:
-    "https://lh3.googleusercontent.com/jfiHJIkvee0QbBbLShykdVSPJ_fNQGsHeUcBgwem4OXxb2MEA0lkqDzZM8BIWVI5E6836mtYFZ13_PhWdkiIuUvGitu5JGDABdzDjQ=w600",
+    "https://lh3.googleusercontent.com/pw/AM-JKLW8fy0VuN3j0aB84c85aXtNm8Oantg78SUSgQedjLPW1UToF_GGkFX34_mf67rbOnSxRdgSubY3VPdMkGmmFS8LZZw9tIpXsj7hMwWK03wn5vHJaTMMpT2opjTHpZpdwUTFHsT702cNYpVVK_q3-DQ=w1012-h1007-no?authuser=1",
   btnUrl: "https://opensea.io/",
   alt: "Back Card",
   profileAlt: "My Profile 2",
@@ -74,27 +74,27 @@ export const backCard = {
 export const galleryHeading = "Gallery"
 export const images = [
   {
-    url: "https://lh3.googleusercontent.com/x8bnseXn89ubTHYxH_tqCni8pukbQGdsAdV-k5sudEFvukpeBxtzMrOSUR00x6lO_Bq5mlgY0mNHvwXw8iSn6ZUBEOCKvUMTzjsXng=w600",
+    url: "https://lh3.googleusercontent.com/pw/AM-JKLW8fy0VuN3j0aB84c85aXtNm8Oantg78SUSgQedjLPW1UToF_GGkFX34_mf67rbOnSxRdgSubY3VPdMkGmmFS8LZZw9tIpXsj7hMwWK03wn5vHJaTMMpT2opjTHpZpdwUTFHsT702cNYpVVK_q3-DQ=w1012-h1007-no?authuser=1",
     alt: "nft1",
   },
   {
-    url: "https://lh3.googleusercontent.com/SjjTt1xeqjYXIPXOGDE5JJuMBFB7EXnwQTxXE_zQE6VnsrJBOtC0kVF254LUHV6RFIH9gyLw0huL3BFQ10N8VWolN_knN0E3qU3z3Q=w600",
+    url: "https://lh3.googleusercontent.com/pw/AM-JKLW8fy0VuN3j0aB84c85aXtNm8Oantg78SUSgQedjLPW1UToF_GGkFX34_mf67rbOnSxRdgSubY3VPdMkGmmFS8LZZw9tIpXsj7hMwWK03wn5vHJaTMMpT2opjTHpZpdwUTFHsT702cNYpVVK_q3-DQ=w1012-h1007-no?authuser=1",
     alt: "nft2",
   },
   {
-    url: "https://lh3.googleusercontent.com/yHkmkqhp_8pRtrp0UnvEJqjMNn-9a9d0xdnuOY-eprnqNG9m2BVJfvd-6tTUWb6_MdqfUFP5FFy4OGqTq9NTHpYybemg5801LKH9eA=w600",
+    url: "https://lh3.googleusercontent.com/pw/AM-JKLW8fy0VuN3j0aB84c85aXtNm8Oantg78SUSgQedjLPW1UToF_GGkFX34_mf67rbOnSxRdgSubY3VPdMkGmmFS8LZZw9tIpXsj7hMwWK03wn5vHJaTMMpT2opjTHpZpdwUTFHsT702cNYpVVK_q3-DQ=w1012-h1007-no?authuser=1",
     alt: "nft3",
   },
   {
-    url: "https://lh3.googleusercontent.com/aviADDqmYN5X979x0bO1Tg7lLLgpIRwp5B-M2GRmChTdm8N5V_vwXX0kS8bTeuATkHlPDsHyeVAgiAhRhHCwzThqTCvn_80r9VNq9A=w600",
+    url: "https://lh3.googleusercontent.com/pw/AM-JKLW8fy0VuN3j0aB84c85aXtNm8Oantg78SUSgQedjLPW1UToF_GGkFX34_mf67rbOnSxRdgSubY3VPdMkGmmFS8LZZw9tIpXsj7hMwWK03wn5vHJaTMMpT2opjTHpZpdwUTFHsT702cNYpVVK_q3-DQ=w1012-h1007-no?authuser=1",
     alt: "nft4",
   },
   {
-    url: "https://lh3.googleusercontent.com/sCYqk7lTMznAOD8Mjlzce_YOw8QR23ODIspxz2X6_gSULHb0QbDapw2brHd2Gf-O9voLXUgSwGWQ7BAsVtsL4tgw9w90ybQBoeY8SQ=w600",
+    url: "https://lh3.googleusercontent.com/pw/AM-JKLW8fy0VuN3j0aB84c85aXtNm8Oantg78SUSgQedjLPW1UToF_GGkFX34_mf67rbOnSxRdgSubY3VPdMkGmmFS8LZZw9tIpXsj7hMwWK03wn5vHJaTMMpT2opjTHpZpdwUTFHsT702cNYpVVK_q3-DQ=w1012-h1007-no?authuser=1",
     alt: "nf5",
   },
   {
-    url: "https://lh3.googleusercontent.com/6IYRsI8QSUQ0dhn11OVmyA0xMZlQDVLM6ORMZL-DVNTquWkSKuw2Xombc3gUJ-bvnQ0r7WJ3TWky-LWsRXO1k8lnOIy1ttt4Sx_r5Y8=w600",
+    url: "https://lh3.googleusercontent.com/pw/AM-JKLW8fy0VuN3j0aB84c85aXtNm8Oantg78SUSgQedjLPW1UToF_GGkFX34_mf67rbOnSxRdgSubY3VPdMkGmmFS8LZZw9tIpXsj7hMwWK03wn5vHJaTMMpT2opjTHpZpdwUTFHsT702cNYpVVK_q3-DQ=w1012-h1007-no?authuser=1",
     alt: "nft6",
   },
 ]
@@ -164,38 +164,26 @@ export const roadmapSteps = [
 export const teamHeading = "Team"
 export const team = [
   {
-    url: "https://lh3.googleusercontent.com/x8bnseXn89ubTHYxH_tqCni8pukbQGdsAdV-k5sudEFvukpeBxtzMrOSUR00x6lO_Bq5mlgY0mNHvwXw8iSn6ZUBEOCKvUMTzjsXng=w600",
+    url: "https://lh3.googleusercontent.com/pw/AM-JKLW8fy0VuN3j0aB84c85aXtNm8Oantg78SUSgQedjLPW1UToF_GGkFX34_mf67rbOnSxRdgSubY3VPdMkGmmFS8LZZw9tIpXsj7hMwWK03wn5vHJaTMMpT2opjTHpZpdwUTFHsT702cNYpVVK_q3-DQ=w1012-h1007-no?authuser=1",
     alt: "user1",
     username: "@username",
     userLink: "https://twitter.com/elonmusk",
   },
   {
-    url: "https://lh3.googleusercontent.com/SjjTt1xeqjYXIPXOGDE5JJuMBFB7EXnwQTxXE_zQE6VnsrJBOtC0kVF254LUHV6RFIH9gyLw0huL3BFQ10N8VWolN_knN0E3qU3z3Q=w600",
+    url: "https://lh3.googleusercontent.com/pw/AM-JKLW8fy0VuN3j0aB84c85aXtNm8Oantg78SUSgQedjLPW1UToF_GGkFX34_mf67rbOnSxRdgSubY3VPdMkGmmFS8LZZw9tIpXsj7hMwWK03wn5vHJaTMMpT2opjTHpZpdwUTFHsT702cNYpVVK_q3-DQ=w1012-h1007-no?authuser=1",
     alt: "user2",
     username: "@username",
     userLink: "https://twitter.com/elonmusk",
   },
   {
-    url: "https://lh3.googleusercontent.com/yHkmkqhp_8pRtrp0UnvEJqjMNn-9a9d0xdnuOY-eprnqNG9m2BVJfvd-6tTUWb6_MdqfUFP5FFy4OGqTq9NTHpYybemg5801LKH9eA=w600",
+    url: "https://lh3.googleusercontent.com/pw/AM-JKLW8fy0VuN3j0aB84c85aXtNm8Oantg78SUSgQedjLPW1UToF_GGkFX34_mf67rbOnSxRdgSubY3VPdMkGmmFS8LZZw9tIpXsj7hMwWK03wn5vHJaTMMpT2opjTHpZpdwUTFHsT702cNYpVVK_q3-DQ=w1012-h1007-no?authuser=1",
     alt: "user3",
     username: "@username",
     userLink: "https://twitter.com/elonmusk",
   },
   {
-    url: "https://lh3.googleusercontent.com/aviADDqmYN5X979x0bO1Tg7lLLgpIRwp5B-M2GRmChTdm8N5V_vwXX0kS8bTeuATkHlPDsHyeVAgiAhRhHCwzThqTCvn_80r9VNq9A=w600",
+    url: "https://lh3.googleusercontent.com/pw/AM-JKLW8fy0VuN3j0aB84c85aXtNm8Oantg78SUSgQedjLPW1UToF_GGkFX34_mf67rbOnSxRdgSubY3VPdMkGmmFS8LZZw9tIpXsj7hMwWK03wn5vHJaTMMpT2opjTHpZpdwUTFHsT702cNYpVVK_q3-DQ=w1012-h1007-no?authuser=1",
     alt: "user4",
-    username: "@username",
-    userLink: "https://twitter.com/elonmusk",
-  },
-  {
-    url: "https://lh3.googleusercontent.com/sCYqk7lTMznAOD8Mjlzce_YOw8QR23ODIspxz2X6_gSULHb0QbDapw2brHd2Gf-O9voLXUgSwGWQ7BAsVtsL4tgw9w90ybQBoeY8SQ=w600",
-    alt: "user5",
-    username: "@username",
-    userLink: "https://twitter.com/elonmusk",
-  },
-  {
-    url: "https://lh3.googleusercontent.com/6IYRsI8QSUQ0dhn11OVmyA0xMZlQDVLM6ORMZL-DVNTquWkSKuw2Xombc3gUJ-bvnQ0r7WJ3TWky-LWsRXO1k8lnOIy1ttt4Sx_r5Y8=w600",
-    alt: "user6",
     username: "@username",
     userLink: "https://twitter.com/elonmusk",
   },

5fb8f438324dc8feefc00979caac0fba7c5cd5cc
NeterNFT <104866216+neternnft@users.noreply.github.com>
2023-10-28 01:03:17 +0200

    gg

---
+++ constants/constants.js
@@ -0,0 +1,182 @@
+/*
+  Hello, change the text inside these variables to update
+  the content of the website.
+
+  For images, you can type the path to a file inside
+  the images folder in /public/images. To do that, 
+  just type "/images/[imageName].extension" inside
+  the variable. Otherwise, you can insert an url
+  from a website, and in this case you need to include
+  that domain in next.config.js
+
+  Thank You!
+*/
+
+// ---GENERAL---
+export const logo = "/images/logoN.png"
+export const logoAlt = "nft project logo"
+export const mobileMenuHeading = "Useful Links:"
+
+export const discordUrl = "https://discord.gg/XDJZcHAVsm"
+export const instagramUrl = "https://www.instagram.com/netern.nft/"
+export const twitterUrl = "https://twitter.com/Neternft"
+export const openSeaUrl = "https://twitter.com/Neternft"
+
+// ---NAVBAR---
+export const pages = [
+  { name: "Gallery", id: "#gallery" },
+  { name: "Game", id: "#game" },
+  { name: "Features", id: "#features" },
+  { name: "Roadmap", id: "#roadmap" },
+  { name: "Team", id: "#team" },
+  { name: "FAQ", id: "#faq" },
+]
+
+// ---FOOTER---
+export const permissions = "2022 Netern"
+
+// ---HERO TEXT---
+export const heading1 = "Netern"
+export const heading2 = "NFT Project"
+export const description = `
+  Meet a collection of 100+ 
+  figures with different attributes and styles!
+`
+export const btnText1 = "Join Discord"
+export const btnText2 = "Shop on TBA"
+
+export const topCard = {
+  name: "#1",
+  price: "TBA",
+  btnText: "Buy Now",
+  profile:
+    "https://lh3.googleusercontent.com/eLb7t8XfGapgsOi3lYh3avqVP4DQiOglT7YVhnaPK1ZH5mECN64oe6iX-yT8LELFDKzFfRDGJXcpaKiX=s130-rw",
+  image:
+    "https://lh3.googleusercontent.com/EwJIlNrbN5qKBNmkTVZcHyLIoOW5CweClzqB_fZoMR33MHEGHEUgDY6LmbsTumwwsS6yf9QKMj8FrsSnbs3i3BX7uWE6vMeX7yTiftc8F9bCxt-ChnZHAEm2svSxrsJPJ8rO9v8IlsSnHE_89RubSlQrcF86rYgn8KvZLfsPMAQi0luXnYizO0Wduq2sI5v0n90RX2O2gqm0MY44sY9gNNh_p5vLhYdeqLw7n1cgmMuplJC7Uu2jNzThIjo_JA_YL9jTND9FDnH5olyFnAEq-1C1fcoooIDq4C-5KR_G2Kw8gJUJUExvzbyTIir4CJkjCGt-5rAsRg-S0T8mMCX-CNiwKjatmqDWm3UAYBuKomhTXt_YFWTRubxL6JCI2KVlO1X8RZnZVu8KskRP0UCaSZDz86YWDbeW8Uu1mN6aO-sTwq566e0C1OQeaRWEAi2ePTvRaZm1fcVK1xhy7JZld9YjL1xwG0uwBX0LxAO0AicreOLiQvsnyccDIctceKtTCe6AZ648loqASl0hm3AyW1yXPF2HCudiH2M3MEn2N3-Ilk2cIRWrZF4JDY5ufdvG4RgNtbSvEZlyILi-aY1N_UyeT_CFIdfQU7CUTfUTKLnWpjx0IkGfxOXdQAq4a4ZAKhbH1SF2X3w1tGW25IMvSA1uBiR6ViNQF1Jbp2X-0lCSTEzxId-yCoSpOn5whHzGoBns26IzL-2wjLpm7imT1sOK4eGNlhcx8E_nYWT-FGP_t5hDpRKyq2ffeegV24GVWYRyuDAZfW5pdPKnQsaEs-iNWVv_f_DOPUPEvAKT7ERNVbDUpT3zOwkkM2GIZNkMLG5Z1N9O3eGiOM6QuIyHQhbenhytF9NnPFS4VlH_viovuDoi7VtJB_XlcEfjbgedoVIdMaLPnj8asyp39985tc_VWjwS-E8COlJuO9dluzqpJkuF6pyHlbZfnH6B4RdpYI1IzB2sfOAExbv8qRD9eA=s1007-no?authuser=1",
+  btnUrl: "https://twitter.com/Neternft",
+  alt: "Top Card",
+  profileAlt: "My Profile 1",
+}
+
+export const backCard = {
+  name: "#2",
+  price: "TBA",
+  btnText: "Buy Now",
+  profile:
+    "https://lh3.googleusercontent.com/eLb7t8XfGapgsOi3lYh3avqVP4DQiOglT7YVhnaPK1ZH5mECN64oe6iX-yT8LELFDKzFfRDGJXcpaKiX=s130-rw",
+  image:
+    "https://lh3.googleusercontent.com/tiOdll8C57CGuoHdIuQdoTOX_aw6Ts_xbjmkKaimrTSUx-38F07vVxLTDPz1uzXAQ8QhQnnMQY_e4p1fCt6a2-aO1IBG2lfsJV3bBj1eYpo8oLiBdFeG3kRtQdRei6kbms3I1yB7CalGDUSZ7k3ksIm0CLVl_ZbQIcJeFPKE4-5mp-2M0yPv9KQDuqU2AkKd15iO1AKhbkvFaDQ6l0Vj5djesOS2V-6vr6_BQyX_8unT2kzai6ywa8_ohCn1fx6czW6uGXtRg32Qy_E8ju_zQt-jW7N_wR6aujX4ogjmBnZAsKJdWt9J5-dq5V55xMg1jOQTTLh5LoCd45GgIXrP1JVc0fngROv3GfBqtsYIgzdm3wqYQkYNgBZPmdM8TvhwSO9wDYiha8V7sGdIB9-4lOZg6Xe0j-eCv6GbBkLZY1Ao1kf9lFrrsSnzv9dbYOua_hXNvorY5euSVvAM1JwNZRZ5fl50TKbMgicgXNNo-uxCjzxcUUsRIJ4qT45Q_fyqmHchprnRDpcF-6tX0StS9fDnfayFxsMacO0kD1SOijB3pheK5W1bNLET_zHd5pf83zUUHm4Z33Bh2eBT_gib450W0NXMTIX3mPWRPnDZP54CFkAcV_BHGJ0jy04FhUqoXMDgUXKggibl8zhbdiuFCl0riTUw1p2oSt9N9RzkxJFX28VnFgP1v4Bwfw8K5yAxgcSaM5uil1cz9RyiECHbBBKYDo8OdY9R1Su4NSJKxdgO14rhcc9F7c3jyw_FzUXbQDF8eJDaVvOcFWCqsLbzcsUlZLq0kotb5WQ-pgD-8ZKl9orUqjVPjVRCGJZVX0X1R5X_rH-gkzjfHYZOzY18e0_YT_THtLvOa2NZ3xVOzstcVg-StVJx-5yDacEcy-1A-FLNBhe90cEkk0EqKGPRFqJPrRfw-wp6jnizHFl3gU13hGs8iJ9_MTlZatKpsvZvjTdpbVBpzUaVhuD_yrMIdw=s1007-no?authuser=1",
+  btnUrl: "https://twitter.com/Neternft",
+  alt: "Back Card",
+  profileAlt: "My Profile 2",
+}
+
+// ---GALLERY---
+export const galleryHeading = "Gallery"
+export const images = [
+  {
+    url: "https://lh3.googleusercontent.com/EwJIlNrbN5qKBNmkTVZcHyLIoOW5CweClzqB_fZoMR33MHEGHEUgDY6LmbsTumwwsS6yf9QKMj8FrsSnbs3i3BX7uWE6vMeX7yTiftc8F9bCxt-ChnZHAEm2svSxrsJPJ8rO9v8IlsSnHE_89RubSlQrcF86rYgn8KvZLfsPMAQi0luXnYizO0Wduq2sI5v0n90RX2O2gqm0MY44sY9gNNh_p5vLhYdeqLw7n1cgmMuplJC7Uu2jNzThIjo_JA_YL9jTND9FDnH5olyFnAEq-1C1fcoooIDq4C-5KR_G2Kw8gJUJUExvzbyTIir4CJkjCGt-5rAsRg-S0T8mMCX-CNiwKjatmqDWm3UAYBuKomhTXt_YFWTRubxL6JCI2KVlO1X8RZnZVu8KskRP0UCaSZDz86YWDbeW8Uu1mN6aO-sTwq566e0C1OQeaRWEAi2ePTvRaZm1fcVK1xhy7JZld9YjL1xwG0uwBX0LxAO0AicreOLiQvsnyccDIctceKtTCe6AZ648loqASl0hm3AyW1yXPF2HCudiH2M3MEn2N3-Ilk2cIRWrZF4JDY5ufdvG4RgNtbSvEZlyILi-aY1N_UyeT_CFIdfQU7CUTfUTKLnWpjx0IkGfxOXdQAq4a4ZAKhbH1SF2X3w1tGW25IMvSA1uBiR6ViNQF1Jbp2X-0lCSTEzxId-yCoSpOn5whHzGoBns26IzL-2wjLpm7imT1sOK4eGNlhcx8E_nYWT-FGP_t5hDpRKyq2ffeegV24GVWYRyuDAZfW5pdPKnQsaEs-iNWVv_f_DOPUPEvAKT7ERNVbDUpT3zOwkkM2GIZNkMLG5Z1N9O3eGiOM6QuIyHQhbenhytF9NnPFS4VlH_viovuDoi7VtJB_XlcEfjbgedoVIdMaLPnj8asyp39985tc_VWjwS-E8COlJuO9dluzqpJkuF6pyHlbZfnH6B4RdpYI1IzB2sfOAExbv8qRD9eA=s1007-no?authuser=1",
+    alt: "nft1",
+  },
+  {
+    url: "https://lh3.googleusercontent.com/tiOdll8C57CGuoHdIuQdoTOX_aw6Ts_xbjmkKaimrTSUx-38F07vVxLTDPz1uzXAQ8QhQnnMQY_e4p1fCt6a2-aO1IBG2lfsJV3bBj1eYpo8oLiBdFeG3kRtQdRei6kbms3I1yB7CalGDUSZ7k3ksIm0CLVl_ZbQIcJeFPKE4-5mp-2M0yPv9KQDuqU2AkKd15iO1AKhbkvFaDQ6l0Vj5djesOS2V-6vr6_BQyX_8unT2kzai6ywa8_ohCn1fx6czW6uGXtRg32Qy_E8ju_zQt-jW7N_wR6aujX4ogjmBnZAsKJdWt9J5-dq5V55xMg1jOQTTLh5LoCd45GgIXrP1JVc0fngROv3GfBqtsYIgzdm3wqYQkYNgBZPmdM8TvhwSO9wDYiha8V7sGdIB9-4lOZg6Xe0j-eCv6GbBkLZY1Ao1kf9lFrrsSnzv9dbYOua_hXNvorY5euSVvAM1JwNZRZ5fl50TKbMgicgXNNo-uxCjzxcUUsRIJ4qT45Q_fyqmHchprnRDpcF-6tX0StS9fDnfayFxsMacO0kD1SOijB3pheK5W1bNLET_zHd5pf83zUUHm4Z33Bh2eBT_gib450W0NXMTIX3mPWRPnDZP54CFkAcV_BHGJ0jy04FhUqoXMDgUXKggibl8zhbdiuFCl0riTUw1p2oSt9N9RzkxJFX28VnFgP1v4Bwfw8K5yAxgcSaM5uil1cz9RyiECHbBBKYDo8OdY9R1Su4NSJKxdgO14rhcc9F7c3jyw_FzUXbQDF8eJDaVvOcFWCqsLbzcsUlZLq0kotb5WQ-pgD-8ZKl9orUqjVPjVRCGJZVX0X1R5X_rH-gkzjfHYZOzY18e0_YT_THtLvOa2NZ3xVOzstcVg-StVJx-5yDacEcy-1A-FLNBhe90cEkk0EqKGPRFqJPrRfw-wp6jnizHFl3gU13hGs8iJ9_MTlZatKpsvZvjTdpbVBpzUaVhuD_yrMIdw=s1007-no?authuser=1",
+    alt: "nft2",
+  },
+  {
+    url: "https://lh3.googleusercontent.com/2ucDVkwxEfAUKA_yU7vQ6M26jM09CDKeW0NE09BAEqvt9aBI7pD5VTqE-8Kh9dB64tlKQSy2TkWXftiuJhsL8nbjZXziXnC7oVY1JvwRjmtCjiYAhXiD4TUhvy8bKzDnQw8jK--lJXQj3VEwoLXG3FjMN2m-e8DzlTmY8ft7zzccNtPNY74e7ih0itglAv5kmgNZStNtfVcJwFylrH1ntgyZh6YbcW2FiZ9KPT0HZP-IGsPmONYhnR27wW0ahx3V5hqlFdzpvdLozIzocyidQC6Bb0HdRYUiHSFe-V74oUdXnl8iQ2u8FzoTSe8j7umX0p65GOo4EKW5HYVHFIa1-il2Bp0K8yq0fk3zGx-q9VuksUyuzOldXNLBHWR0jhcQJsCgWMWspfMqmK5zNnxJLBAPbxGTydq6QVaOeKbIDhC-3Otn4wtkQmVSFh3lpdx8TnDFB2ozY0xxVuZQtLKpc02J8GFdH4FfOCF_MGCrKIxLmj-Aad_v5BiYjpf3twyhZqnw4kjP9ksoYc5ziQiguk7rycaHfOFi-mLUrtFlUazhs4zzpqvzh1s7A46NPHH42qVcG9j9BlPUbiQnv2PpvumEY2swRmu0B7gDyYjBNYYfIHzl27haXZr2NhlLjx82vl_SwSylBZZMMVihn5Lq4Zl9eNeuCSCc9zlwkdkqIy8kFdslfvM9RtQ2iY-hxxO-hUa517VCvwIihjo7TkNbXshFezLvZ2_RV4-mzM8VIXWiIJukd7UMtsigg5gVSCwyKzy0Q3JW7e9CQLI47JFmZQoVyM2TN7IND5IGZSqiCWDof7eBS22mWAUe-_VHSXtaSFm8JB6CXFmI6AlQnPoRK3ch74aU0GpTDeQrLHOTI0mWeFI_qINyh-EQv4_7urLIRpnDVQlSZM58Ix3KUttVHqV-o3gR2KcXfoN_0CU59gKQ_MCfptkth8FdK-fKbESLKzdr6NpJD73316kdS9crdg=s1007-no?authuser=1",
+    alt: "nft3",
+  },
+  {
+    url: "https://lh3.googleusercontent.com/RqVeeaudx-pynPAguXOIcIcgX9_Ts7vmUMhKVdYZ-cluJZKbYRGLx7TrWzJeNpFZJXyvOJ5NMW8hdfiQBCzAUZKfwMELdFWtmZi_pJh4zhHiPa0lkMFEwAAVmw_-N1HBPAnVUxNdU6rezMUQWkSoIux6VMpaXnhuQLxY3J38ChtY8ZzfGSVKq5BKXrBAxtzUBhzOaSR-tePO6MLQn37P8odDhkfKd-23M0t0GiCQHRJIUQgp23xiixFWqHP3subpZMFcS6ZKUD0bopNne7kaSqfc3NrFMe3cHvSTsoK4aSDdErFp1VE3oRvbSEv3FJhgxJjeF8tGfBCrzpa50FqSBvHwrrxDj761kkEEZxvkGf07DMNmVbu5cN1nLILtNlJxFwjH8U2ci1tYd9LecvdvBJ8dX7aa3TEKZ5Jg-ndWjBI7I-Hndh_DAaJRT6aYYFXAlodTvYkwSKzhhtYA0hOgs3APNhG1MzUMe188twVc4mlv7sEda19hTOoUL7h6p1C0Iydr5VV_vllqnS4774y8minmDfDvCkOmT9MFGQZxR0MUfgUuedRnqbNn8k2gRICKkTNWLlz7PYbfG3WjCGdtIEUnKi5hefk5uSGUNkd-XLzYNwCXy37s5L0AZqcHuT1B1ImBlro9sasp3r8peErtCgFQuUGd8EWxLsUOAaYVW0xc57sgCAdwZfXM-ssgn5ct6iAulgbR3z8TB3wDhj_vfFCy9THJOOJno8HDpqvhlcAepK0anVHQA-Chi3oSdKTsFYxxDVT_WwxCNuYm9osEYIzhkn4mE4cP1PM8b1vHnHjv4PQVUvQxIfTVIkXetSrbhdSh1CEt99JNt4ZAbyh87rXmvMGFkCCA_F8aZlnNB9-LgdSs25Mf_A3djjjiWObgSx6Az8SKRlFanRaGIbHRrq-D5Z4DIbK5-UCajprfUoE_FTCDQdPkK0n9YSdIt6OQi6EEFF5lbQGS4wbRpD4PNg=s1007-no?authuser=1",
+    alt: "nft4",
+  },
+  {
+    url: "https://lh3.googleusercontent.com/F3kWwb_hN0it4n3Pss5yc3jPGQbDdHhlPlOl3-nrziMQTzdXp0ALe0kd1NZzrhpLjOrLLjkg-ogRrwp8tFofP4jgXJW3NRQhhVwgKin3tW1SGScUdazzcOJ0D48TVXVmBgJJT6tnUxZp2alGpM0_Dym76rMmhRoSXTqv8gpV5cfsMg6k1scG8XphUrZ_vHARvMTfBmPAw9RvsPVefnM8FtpGK_9Jxdh8Av1-gYOQt_xJ9bxJOAxrlI29WObgllryDj5_Wv02Y8oyS8Kga5JIeiHh4ng6PpqdPLiRXAWiR5b25mRs5pjfGbvFU621kFAY6nZD8pK0xCUFRWBz_txE-g7VY85DyNUxelbq2KSCkmIGO3k0Ww986Jq8GB857Co0Ws3Vsri6mYhEZ0_E63EwfUDvAisgCsb-g-WpFplo69XhZt7JjLxTw7wN4xRiHsymR8U_DxCdFn-qwL-yeIXXAg6_5xe15DZ-vJ35C7i97qLVOKJNXA_YDxXsQ1TKdnJ2G5dZ2HebQw9gOe_5ZStcB3u194iLeM7dsSv2xEHqNAd3BH2-OogUFsJuSjv_dr1kWMrM-lFKQ0yP09AlHkVwTv8i14M1qM2U8mbiS4Uj4_vvvU_5j3eFx9C88QlDaOOpBtjN4fJECJ0Lm-L4hPESB7aIVE96K68UYnWJOFULpo50IdUP2Fn8d4EzsIG9-I9JgaKe8J47p75aoj9UG1UQWkInVd2cCy4VsuNNxP5EwIHYPI1JWOd-KZVssyDi2w6AisqC0Ep-xgXM6XKPkWFUvTFc3_4JYOZRJALWrdCtt0HvywVQhdh-t5Y8l2JBwytold9PlPW04Mm1axl5KdDUeBEIYt99pqYQuYanMM8M3l339hTThq3ih5nXWaqopg1aIl0JtXZXQIOBhuF42dhBEJv2ZXyejJXlPca6MhwoiKoSHreWYP-VH-J1pIEr4Uhw_PET9Hz3SFEHvxZHnH5duQ=s1007-no?authuser=1",
+    alt: "nft5",
+  },
+  {
+    url: "https://lh3.googleusercontent.com/cCKov-g4U8c9Fma2qGEefz1Bynp6wsNCYwYdaT_0qazD4vLTRnmTbrrAwGSTEl6qOlIcidS_pbOa-63oT-KqEwd1d93MmC_TtL-7qG65ItMB9RYJNfy62L1lTDsWHxHu18t8mffYMvw8srTaMGmOz4whnLZNr1jqKyR6iAS4h6rydko4c3FETWRmoN4PnOOQghK2-TvUOxzukLkaWInwK7SKaGBNkF-HeeUczwDGW_AOHQa66m3k6e0Hz1VPvoOAZqNAsDXnT_NevlyzEAqmV5Glst1fHYSmdSwB0yLAgZ9K0XT7upEtLvzZNlaugxFgOqTFmVkPQ-weXmGkQJbTGToA9BomtQDbq49Y8oDWHXsTavMPpammB660Ccc5QQK0FBrDz-NusGckTfVWR3AMoZH2-BzQ8gX2-wRBvgXcYRU3hPF2xoOQUmjsoPqTZuCzyUGkyv_CXnhZy5pj-Lsz8sZSTMJLwP_cvDc3j8VizUktysdbd1Yah3yCqy-6gKJyBmvKA_g4rmDHG-r69IK-xZdYV4X4peP75GG_4N18pURY1YV2BS5WJbp5HlBSjK6GSZDuGh7jrx6qSg1LyHCl6A_bfVOkrOL67vz4t97jmcO-KBaWumWXXJzR1OlCWvBhgzCcGOrAqS4dIApf7PgVABtSVCWHZ4-TtYKxFAgDgNxNyuui2GeGqttk10fsK7vP9HzuA8eFm_aLPA4GT9SBvfxv7t3hBh3ZeE_OEpM-fN-FwPs5ujcQRpBtgLLPFQZIg5QW_yXrEUkOH8qQX7-IXDDWsXtXL--RvilFyEbzb3yy63a1_gJUArTz3Ln_-woCVvQoT-vIe_uc_Braf571fPLKE1k1mZuaDaAy5a-ac7hgQqPmYC0Jvbn3vq6wpHCPJgk78YmKq1y_vb1eQUK5J46-RRCSWkgb7HWqJ0J0VHG_7e7AO7EgH9NxFVTKccboyUqMb2nkuJnR__-1EeD34g=s1007-no?authuser=1",
+    alt: "nft6",
+  },
+]
+
+// ---FEATURES---
+export const features = [
+  {
+    reversed: false,
+    title: "100+ FIgures Collection",
+    description: `After the king created a mysterious orb, 
+      people on Earth are constantly being chased.
+      The King makes an alliance with 100 other Guardians and together 
+      they face the evil enemies of Earth and Heaven.`,
+    image:
+      "https://ouch-cdn2.icons8.com/CCMXcYfslPpvGqO0olPsDxuYq5zjVyHTbhpIbcfN4bQ/rs:fit:735:912/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvOTQ5/LzM3YmUzODE5LTM2/OTEtNDdjNy05YTMz/LWVjMTYwMjA0YTU1/ZS5wbmc.png",
+    imageAlt: "100+ FIgures Collection Image",
+  },
+  {
+    reversed: true,
+    title: "Unique Attributes",
+    description: `Guardians have 50 unique Attributes that are split between female and male figures.
+      25 for female and other 25 for male figures. 
+      Those 50 Attributes are used for 100 nfts.`,
+    image:
+      "https://ouch-cdn2.icons8.com/v6BET7YrOgSq4VCaZKo2L6mVaGxu1oJ0aoH9NzGvKqk/rs:fit:1149:912/czM6Ly9pY29uczgu/b3VjaC1wcm9kLmFz/c2V0cy9wbmcvNTQw/LzVhYTljZWRhLTdm/ZjUtNDM1MC1hNjA0/LWU3OTc1NjZiYTJl/ZC5wbmc.png",
+    imageAlt: "Unique Attributes Image",
+  },
+]
+
+// ---ROADMAP---
+export const roadmapHeading = "Roadmap"
+export const roadmapSteps = [
+  {
+    position: "1",
+    title: "Giveaways and events",
+    description: `
+      Participation in giveaways and events
+      Growing Netern community and opportunity to have a say in the development of the project
+      Distribution of roles and rewards for loyalty
+    `,
+  },
+  {
+    position: "2",
+    title: " Creation of website",
+    description: `
+      Mint : 100 Netern Guardians NFTs are going to be minted on the Oasis blockchain.
+      You have full intellectual property of your Netern Guardian NFTs, so you can use it as a PFP, or for any other purpose.
+    `,
+  },
+  {
+    position: "3",
+    title: "All NFTs Sold",
+    description: `
+      ...
+    `,
+  },
+]
+
+// ---TEAM---
+export const teamHeading = "Team"
+export const team = [
+  {
+    url: "https://lh3.googleusercontent.com/eT_k97dAY_Y5vBfM2O5aMjra_MSIqUEC378UPVhGrX5G4NR1I84V6gJp7CWiwraRMnk6dv4rGND7SHul4eoDp6u8T7CNaFCBG9jfK0CyhQ4sP7sHGDUhE3a4HVvoKVL3sHxVCyN66mR2arqZZmzkLW8EC8UUEeoLG3BqlzRLdGzTJc7Brk1bjk_bjC4WvONhbkFRLoRWErJB51jKFBzxf9tY1tRWIKyx6aNp_YE7louYW5Vp8T-iVtcEnmKXz1akBo4SAjYZ0ejYuwoDPpNc4eYgbJgPebVRjPs5VxAmFE8xiYCXgKRJeO6Np9bBtzrVi2DJE5PlzGnYnqbSbDQb9-aZ2TXf90PpJt5e8jKGAMXsFknUMp6yA3PzjWJzD0jeQnegO2UZCGDmcWetKhCxbXUzlmjzXtKietYygSbjuSm54iPK52uV-Qj8glq6CyddGSspjqRJM5zLnC5n7GGcCDwPwUH5Ak3i0yMkE2a-eQJoG5i6BCJY5w5xeBK8KrSktVa2qMBL0Aa_mjxVyKCZBUanLi1Qq142Xsy1iJTEODcaMPJNjbDiq5eZ4vk0jXbQFZcAKd3srs3D3Gl3G1EY6nJprls33GOzm1i6z-rhZekUz4MRPaa9ltHOrYY5nKdaUjkUzXUPIGf-K9VTISYuehynQsgPfACLKuuGa_b8BQPR0WFs3lfRHze6Dx_Mqra9L9mLEBIVwSY_F7p2FVlN0DdadreaLCsPaJoLc-jznPHBb3ZqNGs2rcTMcj9RkbgIhKZyuJbp26x-VanC8hYiECVjEBhVoO1ipNikhLkH2m7KO5Stbh5EllpGuMHDORuyBQrmAO8CzOOZx5Z5JE54QBR6JIuPZ9LAQDWsjPT5O0Srqc1F7M5MrkUuNv7BW0WS6RDkAKjhkPbqsBFs_smi1xl3AE654UIuyR1-r0sWOdRcz9-IW2GSt80ytiezvvevuGTDkWHXNLPTZWIenRV9=s1007-no?authuser=1",
+    alt: "user1",
+    username: "@Neternft",
+    userLink: "https://twitter.com/Neternft",
+  },
+]
+
+// ---FAQ---
+export const faqHeading = "Frequently Asked Questions"
+export const faq = [
+  {
+    question: "What is Netern?",
+    answer: `Netern is a collection of 100+ amazing figures with
+             different attributes and styles hosted on Emerald Mainnet. 
+    `,
+  },
+  {
+    question: "Can I buy an NFT from TBA?",
+    answer: `Yes, you can buy these figures on TBA.
+    `,
+  },
+]
